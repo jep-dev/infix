b944e827750f908910fb9a94efa9a4dd65177a42
John Petersen <sobeita@gmail.com>
2016-10-10 21:14:50 -0400

    Improved function sorting algorithm

--- src/math/function.cpp
+++ src/math/function.cpp
@@ -12,19 +12,25 @@ namespace Math {
 	}
 	bool function::operator<(function const& f) const
 	{
-		if(f.constant()) {
-			if(constant()) {
-				return (*this)(0,0,0,0,0,0) < f(0,0,0,0,0,0);
-			}
+		auto lconst = constant(), rconst = f.constant();
+		if(lconst && rconst) {
+			return (*this)(0,0,0,0,0,0) < f(0,0,0,0,0,0);
+		} else if(lconst) {
 			return true;
+		} else if(rconst) {
+			return false;
 		}
 		int lcomp = 0, rcomp = 0;
 		for(int i = 0; i < e_param_total; i++) {
 			lcomp = (lcomp << 1) | varies((e_param)i);
 			rcomp = (rcomp << 1) | f.varies((e_param)i);
 		}
+		if(lcomp == rcomp) {
+			return get_type() < f.get_type();
+		}
 		return rcomp < lcomp;
 	}
+
 	function::function(void) {}
 	function::~function(void) {}
 
