bea49a590e8c4e30dcb2ed08303215c972b9da69
John Petersen <sobeita@gmail.com>
2016-10-17 00:33:52 -0400

    Used function::reduce instead of get_terms directly

--- src/math/sum.cpp
+++ src/math/sum.cpp
@@ -62,6 +62,11 @@ namespace Math {
 	}
 	function* sum::reduce(void) const
 	{
+		if(constant())
+			return new value((*lhs)(0,0,0,0,0,0) + (*rhs)(0,0,0,0,0,0));
+		return function::reduce(*this);
+
+		/* -- Old reduce
 		if(constant()) {
 			return new value((*this)(0,0,0,0,0,0));
 		}
@@ -75,48 +80,7 @@ namespace Math {
 			delete r;
 			return l;
 		}
-
-		function *lterms = nullptr, *rterms = nullptr;
-		std::vector<function*> terms;
-		get_terms(*l, terms);
-		get_terms(*r, terms);
-		delete l;
-		delete r;
-		for(auto term : terms) {
-			auto cast = static_cast<const product*>(term);
-			auto lhs_val = (*(cast -> lhs))(0,0,0,0,0,0);
-			if(lhs_val > 0) {
-				function *reduced = term -> reduce();
-				delete term;
-				if(lterms) {
-					lterms = new sum(lterms, reduced);
-				} else {
-					lterms = reduced;
-				}
-			} else {
-				auto cast_rhs = cast -> rhs -> clone();
-				delete term;
-				term = new product(new value(-lhs_val), cast_rhs);
-				if(rterms) {
-					rterms = new sum(rterms, term -> reduce());
-				} else {
-					rterms = term -> reduce();
-				}
-				delete term;
-			}
-		}
-		if(lterms && rterms) {
-			return new difference(lterms, rterms);
-		} else if(lterms) {
-			return lterms;
-		} else if(rterms) {
-			return new negative(rterms);
-		} else {
-			return new value(0);
-		}
-
-		// -- Old reduce
-		/*if(*l == *r) {
+		if(*l == *r) {
 			delete r;
 			auto output = new product(new value(2), l);
 			auto reduced = output -> reduce();
